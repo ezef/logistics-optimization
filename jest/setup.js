a790d18371dae0546af9f615e474bef63220c5dd
Ezequiel Flores <ezequielsebastianflores@gmail.com>
2019-04-23 21:00:57 -0300

    add jest to project

---
+++ jest/setup.js
@@ -0,0 +1,10 @@
+const sails = require('sails');
+const request = require('supertest');
+beforeAll(done => {
+    sails.lift({}, err => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+        global.app = request(sails.hooks.http.app);
+        done(err, sails);
+    });
+});
+afterAll(sails.lower);
